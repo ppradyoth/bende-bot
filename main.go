15a180e9b46b4245c4cd2985d7b7bfaae087ca90
ppradyoth <>
2020-09-27 23:13:20 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "fmt"
+        
+          "log"
+    "net/http"
+  "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func getCompliment() (string, error) {
+    resp, err := http.Get("https://complimentr.com/api")
+    if err != nil {
+        return "", err
+    }
+
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return "", err
+    }
+
+    m := make(map[string]string)
+
+    err = json.Unmarshal(body, &m)
+    if err != nil {
+        return "", err
+    }
+
+    return m["compliment"], nil
+}
+
+func main() {
+
+  bot, err := tgbotapi.NewBotAPI("1268614220:AAGHoP6VShQ8rQzFMycSnvO2L4Y5UgKrxPI")
+    if err != nil {
+        log.Panic(err)
+    }
+
+    bot.Debug = true
+
+    log.Printf("Authorized on account %s", bot.Self.UserName)
+
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates, err := bot.GetUpdatesChan(u)
+
+    for update := range updates {
+        if update.Message == nil { // ignore any non-Message Updates
+            continue
+        }
+
+        log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+    c, err := getCompliment()
+    if err!=nil{
+      fmt.Println("Error while getting compliment")
+      continue
+    }
+
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, c)
+        msg.ReplyToMessageID = update.Message.MessageID
+
+        bot.Send(msg)
+    }
+}
